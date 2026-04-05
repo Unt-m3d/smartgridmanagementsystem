cfb031c92f15a0009abffccab69e9e35731765ec
geofrey getaro maroa(eng-219-074/2024 <reyy56000@gmail.com>
2026-04-05 21:58:18 +0300

    Updated energy/models.py to add input validation for voltage, current, power fields and created a new DeviceStatus model.

--- energy/models.py
+++ energy/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-class EnergyData(models.Model):
-    voltage = models.FloatField()
-    current = models.FloatField()
-    power = models.FloatField()
-    device_on = models.BooleanField(default=True)
+class DeviceStatus(models.Model):
+    device_id = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.power} W"    
+class Energy(models.Model):
+    voltage = models.FloatField(validators=[MinValueValidator(0)])
+    current = models.FloatField(validators=[MinValueValidator(0)])
+    power = models.FloatField(validators=[MinValueValidator(0)])
+    # Other fields can be added here
